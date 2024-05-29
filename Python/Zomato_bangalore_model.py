206270ae5ee4e7de8b0d601009aa551c4926c067
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-05-29 11:04:48 +0700

    Update Zomato_bangalore_model.py

--- Python/Zomato_bangalore_model.py
+++ Python/Zomato_bangalore_model.py
@@ -56,7 +56,7 @@ page_bg_img = '''
     <style>
     [data-testid="stAppViewContainer"]
     {
-    background-image: url("https://plus.unsplash.com/premium_photo-1663852297801-d277b7af6594?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80");
+    background-image: url("https://c4.wallpaperflare.com/wallpaper/317/630/437/artwork-simple-background-blue-stars-wallpaper-preview.jpg");
     background-size: cover;
     }
     </style>
